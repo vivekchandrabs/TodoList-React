9b09d91d50e7b5eccb0e64a2b6960f9c33c61885
Vivek Chandra B S <vivek.chandra@speckbit.com>
2020-05-12 17:54:52 +0530

    Add docs folder

---
+++ docs/precache-manifest.8d15284c3818a4ad486ed30fd0c45222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99df857478cb01aa18d9369864b3a64c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7bbe53eca269c973e6c",
+    "url": "/static/css/main.587b218d.chunk.css"
+  },
+  {
+    "revision": "abc48780b2d628dcd7bc",
+    "url": "/static/js/2.7f723683.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7f723683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7bbe53eca269c973e6c",
+    "url": "/static/js/main.5985a31c.chunk.js"
+  },
+  {
+    "revision": "8f41a90e8619d2bb09e8",
+    "url": "/static/js/runtime-main.11dfc852.js"
+  }
+]);
